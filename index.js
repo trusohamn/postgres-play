56f8ccc6418554bc4cc0c0107f8e2f676c8b7e43
trusohamn <marta.trusohamn@gmail.com>
2019-07-11 12:50:37 +0200

    /db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const db = require('./queries')
 
 app.use(bodyParser.json())
